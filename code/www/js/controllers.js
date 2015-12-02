50f88b859aeedd10a47e5adea65d7140374aede6
Michael Zadra <michaelzadra1@gmail.com>
2015-12-02 12:53:08 -0500

    Adds android emulator capabilities

--- code/www/js/controllers.js
+++ code/www/js/controllers.js
@@ -4,115 +4,109 @@ angular.module('songhop.controllers', ['ionic', 'songhop.services', 'ionic.contr
 /*
 Controller for the discover page
 */
-.controller('DiscoverCtrl', function($scope, User) {
-  var media;
-  var outOfCardsAudio = new Audio('/media/outOfMatches.mp3');
-  // our first three cards
-  var cardTypes = [
-     {
-        "title":"Michael",
-        "artist":"Student at Queen's Univerity",
-        "image_small":"/images/michael_small.jpg",
-        "image":"/images/michael.jpg",
-        "preview_url":"/media/lol.mp3"
-     },
-     {
-        "title":"Anthony",
-        "artist":"Student at Queen's Univerity",
-        "image_small":"/images/anthony_small.jpg",
-        "image":"/images/anthony.jpg",
-        "preview_url":"/media/lol.mp3"
-     },
-     {
-        "title":"Nathaniel",
-        "artist":"Student at Queen's Univerity",
-        "image_small":"/images/nat_small.jpg",
-        "image":"/images/nat.jpg",
-        "preview_url":"/media/lol.mp3"
-     },
-     {
-        "title":"Alex",
-        "artist":"Student at Queen's Univerity",
-        "image_small":"/images/alex_small.jpg",
-        "image":"/images/alex.jpg",
-        "preview_url":"/media/lol.mp3"
-     },
-     {
-        "title":"Aphra",
-        "artist":"Instructor at Queen's Univerity",
-        "image_small":"/images/aphra_small.jpg",
-        "image":"/images/aphra.jpg",
-        "preview_url":"/media/lol.mp3"
-     }
-  ];
-  $scope.cards = Array.prototype.slice.call(cardTypes, 0);
-  
+.controller('DiscoverCtrl', function($scope, User, Cards) {
+    var media;
+    var matchDing = new Audio('media/ding.wav');
 
-  // Play first element audio
-  media = new Audio($scope.cards[0].preview_url);
-  media.play();
-  
-  // Plays audio of next card
-  function playNextAudio(index) {
+    matchDing.addEventListener('ended', function(e){
+      matchDing.pause();
+    }, false);
     
-    // pause current audio
-    media.pause();
-    // play next audio
-    media = new Audio($scope.cards[index].preview_url);
-    media.play();
-  }
-  
-  // Destroy card after every swipe
-  $scope.cardDestroyed = function(index) {
-    $scope.cards.splice(index, 1);
-    // Check if last card was just swiped
-    if ($scope.cards.length != 0){
-      playNextAudio(index);
-    } else {
-      // pause current music
-      media.pause();
-      // play out of cards audio
-      console.log("play end tune");
-      outOfCardsAudio.play();
+    var outOfCardsAudio = new Audio('media/no_cards.m4a');
+
+    // Play first element audio
+    $scope.cards = Cards.queue;
+
+    Cards.playAudio();
+
+    // Plays audio of next card
+    function playNextAudio() {
+        Cards.playAudio();
     }
 
-  }
+    // Destroy card after every swipe
+    function cardDestroyed() {
+        // Update cards
+        Cards.spliceCard();
+        $scope.cards = Cards.queue;
+        // delete current card
 
-  $scope.addCard = function() {
-    var newCard = cardTypes[Math.floor(Math.random() * cardTypes.length)];
-    newCard.id = Math.random();
-    $scope.cards.push(angular.extend({}, newCard));
-  }
+        // Check if last card was just swiped
+        if ($scope.cards.length != 0) {
+            playNextAudio();
+        } else {
+            // pause current music
+            Cards.haltAudio();
+            // play out of cards audio
+            media = outOfCardsAudio;
+            media.play();
+        }
 
-  $scope.cardSwipedLeft = function(index) {
+    }
 
-  }
-  // Add card to matches
-  $scope.cardSwipedRight = function(index) {
-    User.addMatch($scope.cards[index]);
-  }
+    $scope.cardSwipedLeft = function(index) {
+        // pause audio
+        Cards.haltAudio();
+        // destroy card
+        cardDestroyed();
 
-  
+    }
+    // Add card to matches
+    $scope.cardSwipedRight = function(index) {
+        // pause audio
+        Cards.haltAudio();
+        // add to matches
+        if ($scope.cards[index].liked_you == true) {
+            User.addMatch($scope.cards[index]);
+            matchDing.play();
+            
+        }
+        // destroy card
+        cardDestroyed();
+    }
+
+    $scope.playName = function(index) {
+        console.log("Double tap");
+    }
+
 })
 
 
 /*
 Controller for the favorites page
 */
-.controller('FavoritesCtrl', function($scope, User) {
-  
-  // Get list of matches
-  $scope.matches = User.matches;
-  // remove a match
-  $scope.deleteMatch = function(song, index){
-    User.removeMatch(song, index);
-  }
+.controller('FavoritesCtrl', function($scope, User, Cards) {
+    var bioMedia;
+    $scope.cards = Cards.queue[0].preview_url;
+
+    // Get list of matches
+    $scope.matches = User.matches;
+    // remove a match
+    $scope.deleteMatch = function(song, index) {
+        User.removeMatch(song, index);
+    }
+    $scope.playBio = function(index) {
+        // check if a bio is already playing
+        if (bioMedia) bioMedia.pause();
+        // play selected person's bio
+        bioMedia = new Audio($scope.matches[index].bio_url);
+        bioMedia.play();
+    }
 
 })
 
 /*
 Controller for our tab bar
 */
-.controller('TabsCtrl', function($scope) {
+.controller('TabsCtrl', function($scope, Cards) {
+    $scope.enteringMatches = function() {
+        Cards.haltAudio();
+    }
+    $scope.enteringDiscovery = function() {
+        if (Cards.queue[0].title != "Michael") {
+            Cards.playAudio();
+        }
+
+    }
 
 })
--- code/www/js/services.js
+++ code/www/js/services.js
@@ -1,14 +1,14 @@
 angular.module('songhop.services', [])
     .factory('User', function() {
         var o = {
-                matches: [] // matches
-            }
+            matches: [] // matches
+        }
 
         // Add person to matches (card)
         o.addMatch = function(match) {
-        	// if out of cards
+            // if out of cards
             if (!match) {
-            	return false;
+                return false;
             }
             // append to matches array
             o.matches.push(match);
@@ -26,9 +26,71 @@ angular.module('songhop.services', [])
         return o;
     })
     .factory('Cards', function() {
-    	var o = {
-    		queue: []
-    	};
+        // cards
+        var outOfCardsAudio = new Audio('/media/no_cards.m4a');
+        var media;
+        var o = {
+            queue: [{
+                "title": "Michael",
+                "artist": "Student at Queen's University",
+                "image_small": "images/michael_small.jpg",
+                "image": "images/michael.jpg",
+                "preview_url": "media/michael_preview.m4a",
+                "bio_url": "media/bio.mp3",
+                "liked_you": false
+            }, {
+                "title": "Anthony",
+                "artist": "Student at Queen's University",
+                "image_small": "images/anthony_small.jpg",
+                "image": "images/anthony.jpg",
+                "preview_url": "media/anthony_intro.m4a",
+                "bio_url": "media/anthony_bio.m4a",
+                "liked_you": true
+            }, {
+                "title": "Nathaniel",
+                "artist": "Student at Queen's University",
+                "image_small": "images/nat_small.jpg",
+                "image": "images/nat.jpg",
+                "preview_url": "media/nat_intro.m4a",
+                "bio_url": "media/bio.mp3",
+                "liked_you": false
+            }, {
+                "title": "Alex",
+                "artist": "Student at Queen's University",
+                "image_small": "images/alex_small.jpg",
+                "image": "images/alex.jpg",
+                "preview_url": "media/alex_intro.m4a",
+                "bio_url": "media/alex_bio.m4a",
+                "liked_you": true
+            }, {
+                "title": "Taylor",
+                "artist": "Instructor at Queen's University",
+                "image_small": "images/taylor_small.jpg",
+                "image": "images/taylor.jpg",
+                "preview_url": "media/taylor_intro.m4a",
+                "bio_url": "media/bio.mp3",
+                "liked_you": false
+            }]
+        };
+        
+        o.spliceCard = function() {
+            // update cards when card is destroyed
+            o.queue.splice(0, 1);
+            media.pause();
 
-    	return 0;
+        }
+        o.playAudio = function() {
+            media = new Audio(o.queue[0].preview_url);
+            media.play();
+        }
+        o.haltAudio = function(){
+            media.pause();
+
+        }
+        o.pauseCurrentAudio = function(){
+            media = new Audio(o.queue[0].preview_url);
+            media.pause();
+        }
+
+        return o;
     })
